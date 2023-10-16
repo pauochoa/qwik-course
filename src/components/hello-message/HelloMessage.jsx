76fa2aa6dd3e985c0395e90f2b63d3c25f564ba7
Pau Ochoa <pau8av@gmail.com>
2023-10-16 09:50:06 -0600

    Ajustes curso

---
+++ src/components/hello-message/HelloMessage.jsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+
+
+const HelloMessage = component$(() => {
+
+    return (
+        <h1>
+            Welcome to qwik
+        </h1>
+    );
+
+});
+
+export default HelloMessage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,2 +1,18 @@
+import { component$ } from "@builder.io/qwik";
+import { HelloMessage } from "../components/hello-message/HelloMessage";
 
+export default component$(() => {
 
+    console.log("Initializing HelloWorld component");
+    return(
+       <HelloMessage/>
+    );
+ 
+
+});
+
+export const HelloMessage = component$(() => {
+    return (
+        <h1>Welcome to this Qwik course!</h1>
+    );
+});
